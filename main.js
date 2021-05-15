7b1a19c6007d867cde82770b4a24413eb9b84f2c
namnhi997 <namnhi.vnu@gmail.com>
2021-05-15 21:47:39 +0700

    update

--- main.js
+++ main.js
@@ -41,11 +41,13 @@ function setTimess() {
     const hour = new Date().getHours();
     const minute = new Date().getMinutes();
     const second = new Date().getSeconds();
+    const day = new Date().getDay() + 1;
+    console.log(day);
     // export data
     hours.innerHTML = (hour < 10) ? '0' + hour : hour;
     minutes.innerHTML = (minute < 10) ? '0' + minute : minute;
     seconds.innerHTML = (second < 10) ? '0' + second : second;
-    if (hour === 7 && minute === 45 && second === 0) {
+    if (hour === 7 && minute === 45 && second === 0 && day <= 7) {
         /**
          * New Data auto
          */
@@ -58,7 +60,7 @@ function setTimess() {
         request.send(formData);
         const modifi = document.querySelector('.app_desc');
 
-    } else if (hour === 12 && minute === 00 && second === 0) {
+    } else if (hour === 12 && minute === 00 && second === 0 && day <= 7) {
         /**
          * New Data auto
          */
@@ -70,7 +72,7 @@ function setTimess() {
         request.open("POST", "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfbx7aa2dCwLKuo4Ksy5EppGKEx5cOvvD1aXuzJBSj3MjSVxg/formResponse");
         request.send(formData);
 
-    } else if (hour === 17 && minute === 10 && second === 0) {
+    } else if (hour === 17 && minute === 10 && second === 0 && day != 7) {
         /**
          * New Data auto
          */
@@ -86,7 +88,7 @@ function setTimess() {
     }
 
     const d = new Date().getDate();
-    const m = new Date().getMonth();
+    const m = new Date().getMonth() + 1;
     const y = new Date().getFullYear();
 
     dates.innerHTML = d;
