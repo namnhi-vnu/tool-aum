fbdb6dff8ee609287eac7c9d30f6f1473ccfb8ec
namnhi997 <namnhi.vnu@gmail.com>
2021-05-15 11:55:42 +0700

    update

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // select cart html
+
+const dates = document.querySelector('.date');
+const months = document.querySelector('.month');
+const years = document.querySelector('.year');
+
 const hours = document.querySelector('.hour');
 const minutes = document.querySelector('.minute');
 const seconds = document.querySelector('.second');
@@ -9,13 +14,7 @@ const desc = document.querySelector('.desc');
 const hour = new Date().getHours();
 const minute = new Date().getMinutes();
 const second = new Date().getSeconds();
-console.log(desc);
-// active.forEach(item => {
-//     if (hour >= 8 && hour <= 12) {
-//         item.classList.add('active');
-//     }
 
-// });
 for (var i = 0; i <= active.length; i++) {
     if (hour >= 8 && hour < 12) {
         active[0].classList.add('active');
@@ -52,7 +51,7 @@ function setTimess() {
         request.send(formData);
         const modifi = document.querySelector('.app_desc');
 
-    } else if (hour === 12 && minute === 10 && second === 0) {
+    } else if (hour === 12 && minute === 00 && second === 0) {
         /**
          * New Data auto
          */
@@ -73,32 +72,18 @@ function setTimess() {
         formData.append('entry.718595202', 'Dev');
         formData.append('entry.1580319989', 'Tan Làm');
 
-
         var request = new XMLHttpRequest();
         request.open("POST", "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfbx7aa2dCwLKuo4Ksy5EppGKEx5cOvvD1aXuzJBSj3MjSVxg/formResponse");
         request.send(formData);
 
     }
 
+    const d = new Date().getDate();
+    const m = new Date().getMonth();
+    const y = new Date().getFullYear();
+
+    dates.innerHTML = d;
+    months.innerHTML = m;
+    years.innerHTML = y;
 }
 setInterval(setTimess, 1000);
-
-
-
-
-
-
-
-
-
-
-
-/*
-function ghiNgayChamCong() {
-    const dateCham = new Date();
-    const fs = require('fs');
-    const data = 'my name Alex';
-    fs.writeFile('./content/data.txt', data, (err) => {
-        if (err) throw err;
-    });
-}*/
